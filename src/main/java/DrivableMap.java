1ce561e38a873a7f341f8113903e44f57b8124bb
Max Chen <maxlolm1k2l3@gmail.com>
2022-10-03 22:23:58 -0400

    Finished 04 Assorted

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -28,8 +28,13 @@ class DrivableMap {
      *       Return true if the Drivable was added to drivable_map.
      */
 
-
-
+    public boolean addDrivable(String id, Drivable obj) {
+        if(drivable_map.containsKey(id)){
+            return false;
+        }
+        drivable_map.put(id, obj);
+        return true;
+    }
 
     /* TODO: Write a method named hasFasterThan that takes an int (a speed)
      *       and returns true iff there is at least one item in drivable_map
@@ -37,7 +42,14 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
-
+    public boolean hasFasterThan(int speed){
+        for(Drivable item : drivable_map.values()){
+            if(item.getMaxSpeed() >= speed){
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
@@ -46,8 +58,14 @@ class DrivableMap {
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
+    public List<Tradable> getTradable(){
+        List<Tradable> result = new ArrayList<Tradable>();
+        for (Drivable item: drivable_map.values()){
+            if (item instanceof Tradable){
+                result.add((Tradable) item);
+            }
+        }
+        return result;
+    }
 
-
-
-    
 }
